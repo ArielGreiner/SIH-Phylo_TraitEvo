11250ef4adb1a8437c61cf3023cae97f0764259e
ArielGreiner <ariel.kiara@gmail.com>
2016-01-28 10:47:34 -0500

    removed 'browser' code and added 'print' statements in their place

--- run_simtraitevo.R
+++ run_simtraitevo.R
@@ -14,11 +14,18 @@ Scale=rep(c("Local","Regional"),each=length(DispV)*nreplicates*length(TraitEvo))
 MTraits<-t(matrix(1,nspecies,nfunctions))
 for(l in 1:length(TraitEvo)){
 	if(l == 2){
-		browser()
+		print(l)
 	}
 for(j in 1:nreplicates){
+		if(j == 25){
+		print(j)
+	}
 	if(j == 50){
-		browser()
+		print(j)
+	}
+	
+		if(j == 25){
+		print(j)
 	}
 #runs the SIH model at all dispersal rates in DispV and saves the abundances and productivity in a list
 set.seed(j)
